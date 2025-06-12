13e94020833d11f028877b36e6e23f692f633dc8
varshil <varshilparejiya2004@gmail.com>
2025-06-12 16:30:10 +0530

    about

---
+++ src/app/[countryCode]/(main)/about/page.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import FeatureCard from "@components/FeatureCard"
+import { Heading } from "@lib/components/ui/heading"
+
+function AboutSection() {
+  return (
+    <section className="bg-[#f9f6f3] text-center py-20 px-4 ">
+      <div className="max-w-4xl mx-auto">
+        <h4 className="text-[11px] tracking-widest text-black-600 uppercase mb-3 font-akzidenz font-thin">
+          About Rino & Pelle
+        </h4>
+        <br />
+        <h2 className="text-4x1 md:text-6xl leading-tight text-black font-thin font-saol">
+          BELIEVE <span className="italic font-saol">in</span> CRAFTSMANSHIP
+          <br />
+          <span className="italic font-saol">and</span> LUXURIOUS DESIGN
+        </h2>
+        <p className="mt-6 text-[18px] font-akzidenz font-thin ">
+          We bring luxurious and contemporary fashion items for wearable prices
+          <br />
+          that enables every woman to achieve that effortless chic lifestyle.
+        </p>
+      </div>
+    </section>
+  )
+}
+
+const FeaturesSection: React.FC = () => {
+  return (
+    <div>
+      <AboutSection></AboutSection>
+      <section className="bg-[#f9f6f3] ">
+        <div className="flex flex-col md:flex-row bg-[#f9f6f3]  p-4 gap-4 justify-center items-center max-w-screen-xl mx-auto">
+          <FeatureCard
+            number="01"
+            title="Detailed Craftsmanship"
+            description="Because of our expertise and passionate participation throughout the entire process we are able to find the perfect balance between the contemporary aesthetic and the belonged quality."
+          />
+          <FeatureCard
+            number="02"
+            title="Design Driven"
+            description="We commit ourselves to move fast in a constantly changing fashion landscape and to incorporate the most relevant trends into our own signature mix of classic and modern elements."
+          />
+          <FeatureCard
+            number="03"
+            title="Affordable Luxury"
+            description="Since 1987 we aim at creating luxurious and contemporary fashion goods for wearable prices, because we believe that every woman should be able to indulge herself in luxury."
+          />
+        </div>
+      </section>
+    </div>
+  )
+}
+export default FeaturesSection
---
+++ src/components/FeatureCard.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+interface FeatureCardProps {
+  number: string
+  title: string
+  description: string
+}
+
+const FeatureCard = ({ number, title, description }: FeatureCardProps) => {
+  return (
+    <div className="relative p-8 bg-[#F5F1EC] flex-1 flex flex-col items-start justify-end overflow-hidden min-h-[400px] aspect-[6/7]">
+      <span className="absolute top-0 right-0 text-[12rem] font-light text-[#E0DDD9] opacity-100 select-none pointer-events-none z-0 leading-none">
+        {number}
+      </span>
+      <h2 className="text-3xl font-saol z-10 relative mt-auto text-[#4A4A4A] sm:w-1/3">
+        {/* {title.split("\n").map((line, index) => (
+          <React.Fragment key={index}>{line}</React.Fragment>
+        ))} */}
+        {title}
+      </h2>
+      <p className="mt-4 text-[rgb(5, 5, 5)] z-10 relative font-akzidenz-grotest pro text-sm ">
+        {description}
+      </p>
+    </div>
+  )
+}
+
+export default FeatureCard

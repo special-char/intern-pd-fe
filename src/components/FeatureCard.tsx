e1f6293a83e2dcfc06793f124337ecb4072e6931
varshil <varshilparejiya2004@gmail.com>
2025-06-12 17:34:43 +0530

    about

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -12,11 +12,16 @@ const FeatureCard = ({ number, title, description }: FeatureCardProps) => {
       <span className="absolute top-0 right-0 text-[12rem] font-light text-[#E0DDD9] opacity-100 select-none pointer-events-none z-0 leading-none">
         {number}
       </span>
-      <h2 className="text-3xl font-saol z-10 relative mt-auto text-[#4A4A4A] sm:w-1/3">
-        {/* {title.split("\n").map((line, index) => (
-          <React.Fragment key={index}>{line}</React.Fragment>
-        ))} */}
-        {title}
+      <h2 className="text-3xl font-saol z-10 relative mt-auto text-[#4A4A4A] sm:w-1/3 font-thin">
+        {title.split(" ").map((word, index) =>
+          index === 1 ? (
+            <em key={index} className="font-saol">
+              {word}
+            </em>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
       </h2>
       <p className="mt-4 text-[rgb(5, 5, 5)] z-10 relative font-akzidenz-grotest pro text-sm ">
         {description}

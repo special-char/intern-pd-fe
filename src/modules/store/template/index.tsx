4daec337b8dbaa5a11fed14630e8fb147ed652ab
kirtipatel45 <patelkirti1445@gmail.com>
2025-06-23 13:56:52 +0530

    Cdn (#45)
    * w-full in hero section
    
    * blog
    
    * checkout
    
    * add

--- src/modules/store/template/index.tsx
+++ src/modules/store/template/index.tsx
@@ -22,13 +22,58 @@ const StoreTemplate = ({
   return (
     <div className="py-6 content-container" data-testid="category-container">
       <div className="w-full">
+        {/* Category Navigation Bar */}
+
         <div className="mt-8 mb-8 text-2xl-semi text-center">
           <h1 data-testid="store-page-title" className="font-thin">
             All Clothing
           </h1>
         </div>
-        {/* Filter & Sort Button */}
         <FilterButton />
+        <div className="flex items-right justify-center  border-b border-gray-200 pb-4 mb-4">
+          <nav className="flex items-center gap-6 overflow-x-auto">
+            <Link
+              href="/store"
+              className="font-bold border-b-2 border-black pb-1"
+            >
+              All Clothing
+            </Link>
+            <span className="text-gray-300">|</span>
+            <Link href="/store/coats" className="hover:underline">
+              Coats
+            </Link>
+            <Link href="/store/jackets" className="hover:underline">
+              Jackets
+            </Link>
+            <Link href="/products/Capes" className="hover:underline">
+              Capes
+            </Link>
+            <Link href="/store/waistcoats" className="hover:underline">
+              Waistcoats
+            </Link>
+            <Link href="/store/sweaters" className="hover:underline">
+              Sweaters
+            </Link>
+            <Link href="/store/cardigans" className="hover:underline">
+              Cardigans
+            </Link>
+            <Link href="/products/dulani-top" className="hover:underline">
+              Tops & Blouses
+            </Link>
+            <Link href="/store/t-shirts" className="hover:underline">
+              T-shirts
+            </Link>
+            <Link href="/store/trousers" className="hover:underline">
+              Trousers
+            </Link>
+            <Link href="/category/Skirts" className="hover:underline">
+              Skirts
+            </Link>
+            <Link href="/store/dresses" className="hover:underline">
+              Dresses
+            </Link>
+          </nav>
+        </div>
         <Suspense fallback={<SkeletonProductGrid />}>
           <PaginatedProducts
             sortBy={sort}

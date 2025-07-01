2c59f15f85a525a05f86205b37131943d9314f50
Kaushal <kaushal151131@gmail.com>
2025-07-01 15:51:17 +0530

    shop page update

--- src/modules/products/templates/index.tsx
+++ src/modules/products/templates/index.tsx
@@ -35,11 +35,7 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
         className="content-container flex flex-col lg:flex-row py-6 relative gap-8"
         data-testid="product-container"
       >
-<<<<<<< HEAD
-=======
-
         {product?.images?.[0] && <EditButton imageId={product.images[0].id} />}
->>>>>>> 8eb181aecf0d124f8280dbcd05bb9c274a1fecd1
         {/* Left Column - Images */}
         <div className="w-full lg:w-2/3">
           <div className="grid grid-cols-2 gap-4">

f18ee6cb092e79d6c10b687a2b138b2d92be23a2
kashyap203 <kashyapvaishnav23@gmail.com>
2025-07-01 14:50:57 +0530

    Kashyapp (#77)
    * shop
    
    * shop
    
    * Refactor SignInDialog to use Dialog component and LoginTemplate for improved UI
    
    * add

--- src/modules/collections/templates/index.tsx
+++ src/modules/collections/templates/index.tsx
@@ -62,6 +62,7 @@ export default async function CollectionTemplate({
             ))}
           </nav>
         </div>
+       
         <Suspense
           fallback={
             <SkeletonProductGrid
